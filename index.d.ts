fb342078cc463538353d8b77a99e012f8aa21fbe
Mingyi_Li <limingyi@baonova.com>
2018-03-13 00:27:05 +0800

    missing

--- index.d.ts
+++ index.d.ts
@@ -2,5 +2,5 @@
 interface Object{
     select : <T>(keys:(keyof T)[]) => {[K in keyof T] :T[K]};
 }
-export function select<U,T>(object : U , keys : (keyof T)[]) : {[K in keyof T] : T[K]};
+export function keyselect<U,T>(object : U , keys : (keyof T)[]) : {[K in keyof T] : T[K]};
 
